816f993479a6470a47cbd59c31cce8bfcb32b0d6
Dan Manchester <dan@elseloop.com>
2023-02-23 11:29:10 -0800

    Fixes clone typo

--- clone.sh
+++ clone.sh
@@ -4,9 +4,9 @@ echo "Cloning repositories..."
 
 SITES=$HOME/Sites
 
-git@github.com:storybookjs/storybook.git $SITES/storybook
-git@github.com:elseloop/storybook-addon-codepen.git $SITES/storybook-addon-codepen
-git@github.com:elseloop/storybook-react-to-static-markup.git $SITES/storybook-react-to-static-markup
+git clone git@github.com:storybookjs/storybook.git $SITES/storybook
+git clone git@github.com:elseloop/storybook-addon-codepen.git $SITES/storybook-addon-codepen
+git clone git@github.com:elseloop/storybook-react-to-static-markup.git $SITES/storybook-react-to-static-markup
 
 # Powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
